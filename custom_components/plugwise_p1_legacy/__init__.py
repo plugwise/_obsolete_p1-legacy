aeb6ceaaceff9f22689b043321f060a69ee8ea42
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2021-07-31 18:20:17 +0200

    Update __init__.py

--- custom_components/plugwise_p1_legacy/__init__.py
+++ custom_components/plugwise_p1_legacy/__init__.py
@@ -1,5 +1,5 @@
 """
 Support for reading energy data from PlugWise Smile P1 via IP.
-This code is meant for a Smile P1 with firmware 2.1.x.
+This code is meant for a Smile P1 with firmware 2.1.x. For P1's with newer firmware use plugwise-beta.
 
 """
