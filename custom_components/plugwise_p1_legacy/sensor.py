b3b4943a3617de459237f8ba8c1291635c230cf3
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2021-07-31 17:31:01 +0200

    Update sensor.py

--- custom_components/plugwise_p1_legacy/sensor.py
+++ custom_components/plugwise_p1_legacy/sensor.py
@@ -1,9 +1,3 @@
-"""
-Support for reading energy data from PlugWise Smile P1 via IP.
-This code is meant for a Smile P1 with firmware 2.1.x.
-
-"""
-
 import logging
 from datetime import timedelta
 import voluptuous as vol
