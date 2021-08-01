d4f0145a454100bbd8b82ca9bd561d9b4d9cc687
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-08-01 10:52:27 +0200

    Black

--- custom_components/plugwise_p1_legacy/sensor.py
+++ custom_components/plugwise_p1_legacy/sensor.py
@@ -6,9 +6,7 @@ from lxml import etree
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import (
-        CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_RESOURCES
-    )
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_RESOURCES
 from homeassistant.util import Throttle
 from homeassistant.helpers.entity import Entity
 
@@ -16,104 +14,187 @@ _LOGGER = logging.getLogger(__name__)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
 
-SENSOR_PREFIX = 'PSP1 '
+SENSOR_PREFIX = "PSP1 "
 SENSOR_TYPES = {
-    'electricity_consumed_offpeak_interval': ['Electricity Consumed Off Peak Interval', 'energy', 'Wh', 'mdi:flash'],
-    'electricity_consumed_peak_interval': ['Electricity Consumed Peak Interval', 'energy' 'Wh', 'mdi:flash'],
-    'electricity_consumed_offpeak_cumulative': ['Electricity Consumed Off Peak Cumulative', 'energy' 'Wh', 'mdi:flash'],
-    'electricity_consumed_peak_cumulative': ['Electricity Consumed Peak Cumulative', 'energy' 'Wh', 'mdi:flash'],
-    'electricity_produced_offpeak_interval': ['Electricity Produced Off Peak Interval', 'energy' 'Wh', 'mdi:white-balance-sunny'],
-    'electricity_produced_peak_interval': ['Electricity Produced Peak Interval', 'energy' 'Wh', 'mdi:white-balance-sunny'],
-    'electricity_produced_offpeak_cumulative': ['Electricity Produced Off Peak Cumulative', 'energy' 'Wh', 'mdi:white-balance-sunny'],
-    'electricity_produced_peak_cumulative': ['Electricity Produced Peak Cumulative', 'energy' 'Wh', 'mdi:white-balance-sunny'],
-    'net_electricity_cumulative': ['Net Electricity Cumulative', 'power', 'W', 'mdi:flash'],
-    'net_electricity_point': ['Net Electricity Point', 'power', 'W', 'mdi:flash'],
-    'gas_consumed_interval': ['Gas Consumed Interval', None, 'm3', 'mdi:gas-cylinder'],
-    'gas_consumed_cumulative': ['Gas Consumed Cumulative', None, 'm3', 'mdi:gas-cylinder'],
+    "electricity_consumed_offpeak_interval": [
+        "Electricity Consumed Off Peak Interval",
+        "energy",
+        "Wh",
+        "mdi:flash",
+    ],
+    "electricity_consumed_peak_interval": [
+        "Electricity Consumed Peak Interval",
+        "energy" "Wh",
+        "mdi:flash",
+    ],
+    "electricity_consumed_offpeak_cumulative": [
+        "Electricity Consumed Off Peak Cumulative",
+        "energy" "Wh",
+        "mdi:flash",
+    ],
+    "electricity_consumed_peak_cumulative": [
+        "Electricity Consumed Peak Cumulative",
+        "energy" "Wh",
+        "mdi:flash",
+    ],
+    "electricity_produced_offpeak_interval": [
+        "Electricity Produced Off Peak Interval",
+        "energy" "Wh",
+        "mdi:white-balance-sunny",
+    ],
+    "electricity_produced_peak_interval": [
+        "Electricity Produced Peak Interval",
+        "energy" "Wh",
+        "mdi:white-balance-sunny",
+    ],
+    "electricity_produced_offpeak_cumulative": [
+        "Electricity Produced Off Peak Cumulative",
+        "energy" "Wh",
+        "mdi:white-balance-sunny",
+    ],
+    "electricity_produced_peak_cumulative": [
+        "Electricity Produced Peak Cumulative",
+        "energy" "Wh",
+        "mdi:white-balance-sunny",
+    ],
+    "net_electricity_cumulative": [
+        "Net Electricity Cumulative",
+        "power",
+        "W",
+        "mdi:flash",
+    ],
+    "net_electricity_point": ["Net Electricity Point", "power", "W", "mdi:flash"],
+    "gas_consumed_interval": ["Gas Consumed Interval", None, "m3", "mdi:gas-cylinder"],
+    "gas_consumed_cumulative": [
+        "Gas Consumed Cumulative",
+        None,
+        "m3",
+        "mdi:gas-cylinder",
+    ],
 }
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string,
-    vol.Required(CONF_RESOURCES, default=[]):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Required(CONF_RESOURCES, default=[]): vol.All(
+            cv.ensure_list, [vol.In(SENSOR_TYPES)]
+        ),
+    }
+)
 
-class PlugWiseP1Api():
 
+class PlugWiseP1Api:
     def __init__(self, host, user, password):
         self.session = requests.Session()
         self.host = host
         self.user = user
         self.password = password
-    
+
     def get_base_url(self):
-        return 'http://{user}:{password}@{host}/'.format(user=self.user, password=self.password, host=self.host)
+        return "http://{user}:{password}@{host}/".format(
+            user=self.user, password=self.password, host=self.host
+        )
 
     def get_electricity_module(self):
-        modules_response = self.session.get(self.get_base_url() + 'core/modules')
+        modules_response = self.session.get(self.get_base_url() + "core/modules")
         modules_xml = etree.fromstring(modules_response.content)
-        module_id = modules_xml.xpath('//modules/module[count(./services/electricity_interval_meter)>0]/@id')[0]
-        module_response = self.session.get(self.get_base_url() + 'core/modules;id={module_id}'.format(module_id=module_id))
+        module_id = modules_xml.xpath(
+            "//modules/module[count(./services/electricity_interval_meter)>0]/@id"
+        )[0]
+        module_response = self.session.get(
+            self.get_base_url()
+            + "core/modules;id={module_id}".format(module_id=module_id)
+        )
         module_xml = etree.fromstring(module_response.content)
         return module_xml
 
     def get_gas_module(self):
-        modules_response = self.session.get(self.get_base_url() + 'core/modules')
+        modules_response = self.session.get(self.get_base_url() + "core/modules")
         modules_xml = etree.fromstring(modules_response.content)
-        module_id = modules_xml.xpath('//modules/module[count(./services/gas_interval_meter)>0]/@id')[0]
-        module_response = self.session.get(self.get_base_url() + 'core/modules;id={module_id}'.format(module_id=module_id))
+        module_id = modules_xml.xpath(
+            "//modules/module[count(./services/gas_interval_meter)>0]/@id"
+        )[0]
+        module_response = self.session.get(
+            self.get_base_url()
+            + "core/modules;id={module_id}".format(module_id=module_id)
+        )
         module_xml = etree.fromstring(module_response.content)
         return module_xml
 
     def get_electricity_consumed_point(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_point_meter/measurement[@directionality="consumed"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_point_meter/measurement[@directionality="consumed"]/text()'
+        )[0]
         return float(value)
-    
+
     def get_electricity_consumed_offpeak_interval(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_consumed_peak_interval(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()'
+        )[0]
         return float(value)
-    
+
     def get_electricity_consumed_offpeak_cumulative(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_consumed_peak_cumulative(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_produced_point(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_point_meter/measurement[@directionality="produced"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_point_meter/measurement[@directionality="produced"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_produced_offpeak_interval(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_produced_peak_interval(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_produced_offpeak_cumulative(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()'
+        )[0]
         return float(value)
 
     def get_electricity_produced_peak_cumulative(self, electricity_module):
-        value = electricity_module.xpath('//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()')[0]
+        value = electricity_module.xpath(
+            '//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()'
+        )[0]
         return float(value)
 
     def get_gas_consumed_interval(self, gas_module):
-        value = gas_module.xpath('//module/services/gas_interval_meter/measurement[@directionality="consumed"]/text()')[0]
+        value = gas_module.xpath(
+            '//module/services/gas_interval_meter/measurement[@directionality="consumed"]/text()'
+        )[0]
         return float(value)
 
     def get_gas_consumed_cumulative(self, gas_module):
-        value = gas_module.xpath('//module/services/gas_cumulative_meter/measurement[@directionality="consumed"]/text()')[0]
+        value = gas_module.xpath(
+            '//module/services/gas_cumulative_meter/measurement[@directionality="consumed"]/text()'
+        )[0]
         return float(value)
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the Plugwise Smile sensors."""
@@ -133,12 +214,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         sensor_type = resource.lower()
 
         if sensor_type not in SENSOR_TYPES:
-            SENSOR_TYPES[sensor_type] = [
-                sensor_type.title(), '', 'mdi:flash']
+            SENSOR_TYPES[sensor_type] = [sensor_type.title(), "", "mdi:flash"]
 
         entities.append(PlugwiseSmileSensor(data, sensor_type))
 
     add_entities(entities)
+
 
 # pylint: disable=abstract-method
 class PlugwiseSmileData(object):
@@ -158,40 +239,57 @@ class PlugwiseSmileData(object):
         self._gas_module = self._api.get_gas_module()
 
     def get_electricity_consumed_point(self):
-       return self._api.get_electricity_consumed_point(self._electricity_module)
+        return self._api.get_electricity_consumed_point(self._electricity_module)
 
     def get_electricity_consumed_offpeak_interval(self):
-        return self._api.get_electricity_consumed_offpeak_interval(self._electricity_module)
+        return self._api.get_electricity_consumed_offpeak_interval(
+            self._electricity_module
+        )
 
     def get_electricity_consumed_peak_interval(self):
-        return self._api.get_electricity_consumed_peak_interval(self._electricity_module)
+        return self._api.get_electricity_consumed_peak_interval(
+            self._electricity_module
+        )
 
     def get_electricity_consumed_offpeak_cumulative(self):
-        return self._api.get_electricity_consumed_offpeak_cumulative(self._electricity_module)
+        return self._api.get_electricity_consumed_offpeak_cumulative(
+            self._electricity_module
+        )
 
     def get_electricity_consumed_peak_cumulative(self):
-        return self._api.get_electricity_consumed_peak_cumulative(self._electricity_module)
+        return self._api.get_electricity_consumed_peak_cumulative(
+            self._electricity_module
+        )
 
     def get_electricity_produced_point(self):
         return self._api.get_electricity_produced_point(self._electricity_module)
 
     def get_electricity_produced_offpeak_interval(self):
-        return self._api.get_electricity_produced_offpeak_interval(self._electricity_module)
+        return self._api.get_electricity_produced_offpeak_interval(
+            self._electricity_module
+        )
 
     def get_electricity_produced_peak_interval(self):
-        return self._api.get_electricity_produced_peak_interval(self._electricity_module)
+        return self._api.get_electricity_produced_peak_interval(
+            self._electricity_module
+        )
 
     def get_electricity_produced_offpeak_cumulative(self):
-        return self._api.get_electricity_produced_offpeak_cumulative(self._electricity_module)
+        return self._api.get_electricity_produced_offpeak_cumulative(
+            self._electricity_module
+        )
 
     def get_electricity_produced_peak_cumulative(self):
-        return self._api.get_electricity_produced_peak_cumulative(self._electricity_module)
+        return self._api.get_electricity_produced_peak_cumulative(
+            self._electricity_module
+        )
 
     def get_gas_consumed_interval(self):
         return self._api.get_gas_consumed_interval(self._gas_module)
 
     def get_gas_consumed_cumulative(self):
-        return self._api.get_gas_consumed_cumulative(self._gas_module)    
+        return self._api.get_gas_consumed_cumulative(self._gas_module)
+
 
 class PlugwiseSmileSensor(Entity):
     """Representation of a Plugwise Smile sensor."""
@@ -229,7 +327,7 @@ class PlugwiseSmileSensor(Entity):
     def get_power(self, power_value):
         pvSplit = power_value.split()
         value = float(pvSplit[0])
-        if pvSplit[1] == 'kW':
+        if pvSplit[1] == "kW":
             return value * 1000
         else:
             return value
@@ -237,36 +335,36 @@ class PlugwiseSmileSensor(Entity):
     def update(self):
         """Get the latest data and use it to update our sensor state."""
         self.data.update()
-      
-        if self.type == 'electricity_consumed_offpeak_interval':
+
+        if self.type == "electricity_consumed_offpeak_interval":
             self._state = self.data.get_electricity_consumed_offpeak_interval()
-        elif self.type == 'electricity_consumed_peak_interval':
+        elif self.type == "electricity_consumed_peak_interval":
             self._state = self.data.get_electricity_consumed_peak_interval()
-        elif self.type == 'electricity_consumed_offpeak_cumulative':
+        elif self.type == "electricity_consumed_offpeak_cumulative":
             self._state = self.data.get_electricity_consumed_offpeak_cumulative()
-        elif self.type == 'electricity_consumed_peak_cumulative':
+        elif self.type == "electricity_consumed_peak_cumulative":
             self._state = self.data.get_electricity_consumed_peak_cumulative()
-        elif self.type == 'electricity_produced_offpeak_interval':
+        elif self.type == "electricity_produced_offpeak_interval":
             self._state = self.data.get_electricity_produced_offpeak_interval()
-        elif self.type == 'electricity_produced_peak_interval':
+        elif self.type == "electricity_produced_peak_interval":
             self._state = self.data.get_electricity_produced_peak_interval()
-        elif self.type == 'electricity_produced_offpeak_cumulative':
+        elif self.type == "electricity_produced_offpeak_cumulative":
             self._state = self.data.get_electricity_produced_offpeak_cumulative()
-        elif self.type == 'electricity_produced_peak_cumulative':
+        elif self.type == "electricity_produced_peak_cumulative":
             self._state = self.data.get_electricity_produced_peak_cumulative()
-        elif self.type == 'gas_consumed_interval':
+        elif self.type == "gas_consumed_interval":
             self._state = self.data.get_gas_consumed_interval()
-        elif self.type == 'gas_consumed_cumulative':
+        elif self.type == "gas_consumed_cumulative":
             self._state = self.data.get_gas_consumed_cumulative()
-        elif self.type == 'net_electricity_point':
+        elif self.type == "net_electricity_point":
             self._state = (
-                self.data.get_electricity_consumed_point() 
+                self.data.get_electricity_consumed_point()
                 - self.data.get_electricity_produced_point()
             )
-        elif self.type == 'net_electricity_cumulative':
+        elif self.type == "net_electricity_cumulative":
             self._state = (
-                self.data.get_electricity_consumed_offpeak_cumulative() 
-                + self.data.get_electricity_consumed_peak_cumulative() 
-                - self.data.get_electricity_produced_offpeak_cumulative() 
+                self.data.get_electricity_consumed_offpeak_cumulative()
+                + self.data.get_electricity_consumed_peak_cumulative()
+                - self.data.get_electricity_produced_offpeak_cumulative()
                 - self.data.get_electricity_produced_peak_cumulative()
             )
