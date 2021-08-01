e64fb2deab168cc17b77d6c37e110c47877300d7
Bouwe <bouwe.s.westerdijk@gmail.com>
2021-08-01 19:42:13 +0200

    Move class PlugwiseP1Api into it's own file

--- custom_components/plugwise_p1_legacy/sensor.py
+++ custom_components/plugwise_p1_legacy/sensor.py
@@ -12,6 +12,8 @@ from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_RE
 from homeassistant.util import Throttle
 from homeassistant.helpers.entity import Entity
 
+from .api import PlugWiseP1Api
+
 _LOGGER = logging.getLogger(__name__)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
@@ -104,116 +106,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         ),
     }
 )
-
-
-class PlugWiseP1Api:
-    """ Plugwise P1 API class."""
-
-    def __init__(self, host, user, password):
-        self.session = requests.Session()
-        self.host = host
-        self.user = user
-        self.password = password
-
-    def get_base_url(self):
-        return f"http://{self.user}:{self.password}@{self.host}/"
-
-    def get_electricity_module(self):
-        modules_response = self.session.get(self.get_base_url() + "core/modules")
-        modules_xml = etree.fromstring(modules_response.content)
-        module_id = modules_xml.xpath(
-            "//modules/module[count(./services/electricity_interval_meter)>0]/@id"
-        )[0]
-        module_response = self.session.get(
-            self.get_base_url() + f"core/modules;id={module_id}"
-        )
-        module_xml = etree.fromstring(module_response.content)
-        return module_xml
-
-    def get_gas_module(self):
-        modules_response = self.session.get(self.get_base_url() + "core/modules")
-        modules_xml = etree.fromstring(modules_response.content)
-        module_id = modules_xml.xpath(
-            "//modules/module[count(./services/gas_interval_meter)>0]/@id"
-        )[0]
-        module_response = self.session.get(
-            self.get_base_url() + f"core/modules;id={module_id}"
-        )
-        module_xml = etree.fromstring(module_response.content)
-        return module_xml
-
-    def get_electricity_consumed_point(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_point_meter/measurement[@directionality="consumed"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_consumed_offpeak_interval(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_consumed_peak_interval(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_interval_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_consumed_offpeak_cumulative(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_offpeak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_consumed_peak_cumulative(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_cumulative_meter/measurement[@directionality="consumed" and @tariff_indicator="nl_peak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_produced_point(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_point_meter/measurement[@directionality="produced"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_produced_offpeak_interval(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_produced_peak_interval(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_interval_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_produced_offpeak_cumulative(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_offpeak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_electricity_produced_peak_cumulative(self, electricity_module):
-        value = electricity_module.xpath(
-            '//module/services/electricity_cumulative_meter/measurement[@directionality="produced" and @tariff_indicator="nl_peak"]/text()'
-        )[0]
-        return float(value)
-
-    def get_gas_consumed_interval(self, gas_module):
-        value = gas_module.xpath(
-            '//module/services/gas_interval_meter/measurement[@directionality="consumed"]/text()'
-        )[0]
-        return float(value)
-
-    def get_gas_consumed_cumulative(self, gas_module):
-        value = gas_module.xpath(
-            '//module/services/gas_cumulative_meter/measurement[@directionality="consumed"]/text()'
-        )[0]
-        return float(value)
-
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the Plugwise Smile sensors."""
